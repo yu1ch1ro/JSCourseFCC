9384b12c74ef2e86c4623470a558146ded21e7e2
yuichiro <yosidayuichiro27@gmail.com>
2023-04-05 16:21:50 +0400

    creating the repo and adding the 1st JS Project

---
+++ 1.passengerCounterApp/pca.js
@@ -0,0 +1,22 @@
+let countEl = document.getElementById('count-el')
+let saveEl = document.getElementById('save-el')
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + ' - '
+    saveEl.textContent += countStr
+    countEl.innerText = 0
+    count = 0
+}
+
+function reset() {
+    countEl.textContent = 0
+    count = 0
+    saveEl.textContent = 'Previous entries: '
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# jsCourseFCC
+# full JS Course by freeCodeCamp on YouTube https://www.youtube.com/watch?v=jS4aFq5-91M&t=4856s
